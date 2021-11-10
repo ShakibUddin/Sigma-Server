899b0e9000c4de7bda6e6ef3a9ca0c3f88bf2795
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2021-11-10 06:20:06 +0600

    get and post review done

--- index.js
+++ index.js
@@ -20,21 +20,58 @@ async function run() {
     try {
         await client.connect();
         const database = client.db(process.env.DB_NAME);
-        const productsCollection = database.collection('products');
+        const watchesCollection = database.collection('watches');
+        const usersCollection = database.collection('users');
+        const reviewsCollection = database.collection('reviews');
 
-        //---------------------Products Routes-----------------------------------------
-        // GET  - get products data
-        app.post('/products', async (req, res) => {
-            const totalProductsToShow = req.body.limit;
-            const cursor = totalProductsToShow ? productsCollection.find({}).limit(totalProductsToShow) : productsCollection.find({});
-            const products = await cursor.toArray();
-            res.send(products);
+        //---------------------Watches Routes-----------------------------------------
+        // GET  - get watches data
+        app.get('/watches', async (req, res) => {
+            const cursor = watchesCollection.find({});
+            const watches = await cursor.toArray();
+            res.send(watches);
         });
 
         // POST - saving product in db
         app.post('/product', async (req, res) => {
             const data = req.body;
-            const insertOperation = await productsCollection.insertOne(data);
+            const insertOperation = await watchesCollection.insertOne(data);
+            if (insertOperation.acknowledged) {
+                res.send(true);
+            }
+            else {
+                res.send(false);
+            }
+        });
+
+        //---------------------User Routes-----------------------------------------
+        //UPSERT - update user if exists or insert new
+        app.put('/user', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const upsertOperation = await usersCollection.updateOne(filter, updateDoc, options);
+            if (upsertOperation.acknowledged) {
+                res.send(true);
+            }
+            else {
+                res.send(false);
+            }
+        });
+
+        //---------------------Review Routes-----------------------------------------
+        // GET  - get reviews data
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        // POST - saving product in db
+        app.post('/review', async (req, res) => {
+            const data = req.body;
+            const insertOperation = await reviewsCollection.insertOne(data);
             if (insertOperation.acknowledged) {
                 res.send(true);
             }
